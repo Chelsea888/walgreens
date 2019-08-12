22ea0922f3c1be6bf36f83ef7569f9dd35042939
Cheng Ji <chelseaji1818@gmail.com>
2019-08-11 22:56:58 -0400

    search area

--- getdata.php
+++ getdata.php
@@ -11,8 +11,8 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-#$db = new \PDO('mysql:dbname=walgreens;host=127.0.0.1;charset=utf8mb4', 'wguser', 'AXhf$tu2p5R2');
-$db = new \PDO('mysql:dbname=walgreens;host=localhost;charset=utf8mb4', 'testuser', 'mypassword');
+$db = new \PDO('mysql:dbname=walgreens;host=127.0.0.1;charset=utf8mb4', 'wguser', 'AXhf$tu2p5R2');
+#$db = new \PDO('mysql:dbname=walgreens;host=localhost;charset=utf8mb4', 'testuser', 'mypassword');
 $auth = new \Delight\Auth\Auth($db);
 
 $PROJECTNAME = "walgreens";
@@ -62,3 +62,5 @@ if ($datatype == 'drug') {
     }
     echo json_encode($output);
 }
+
+
